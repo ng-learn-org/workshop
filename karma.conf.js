11ec6a347abc4ac47160b2cf4c8afc18021fd6d2
labrador <labrador@labradoodle.com>
2013-10-14 11:38:01 -0500

    Setup karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,71 @@
 // Karma configuration
+module.exports = function(config) {
+    config.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+        preprocessors: {
+            'app/scripts/**/*.js': 'coverage'
+        },
 
-// list of files to exclude
-exclude = [];
+        frameworks: ['jasmine'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+        // list of files / patterns to load in the browser
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-resource/angular-resource.js',
+//            'app/scripts/**/*.js',
+//            'test/spec/**/*.js',
+            '.tmp/scripts/**/*.js',
+            '.tmp/spec/**/*.js'
+        ],
 
-// web server port
-port = 8080;
+        // list of files to exclude
+        exclude: [],
 
-// cli runner port
-runnerPort = 9100;
+        // test results reporter to use
+        // possible values: dots || progress || growl
+        reporters: ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // web server port
+        port: 8080,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // cli runner port
+        runnerPort: 9100,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: 'LOG_DISABLE',
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 5000,
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
+
+//        coverageReporter: {
+//            type: 'html',
+//            dir: 'coverage/'
+//        }
+
+    });
+};
